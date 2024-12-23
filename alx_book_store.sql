0e920ae527f836f028b7a74d202f22ac7de87354
muhamed9494 <129572431+muhamed9494@users.noreply.github.com>
2024-12-23 02:54:39 +0200

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -14,12 +14,10 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 --
 -- Table structure for table `authors`
---
-CREATE DATABASE IF NOT EXISTS alx_book_store
-  
+--  
 DROP TABLE IF EXISTS `authors`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
