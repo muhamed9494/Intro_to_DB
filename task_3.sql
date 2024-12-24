1b3a8da9262490468b308eae420577fabdd74174
muhamed9494 <129572431+muhamed9494@users.noreply.github.com>
2024-12-24 03:36:10 +0200

    Update task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,6 +1,2 @@
--- Script to list all tables in the provided database
--- Save this file as task_3.sql
-
-SELECT table_name
-FROM information_schema.tables
-WHERE table_schema = DATABASE();
+USE alx_book_store;
+SHOW TABLES;
